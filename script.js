a50ac69f70607a391951c0a4e4023b70aea7e19f
Duog Bach <136550117+duogbachdev@users.noreply.github.com>
2023-07-02 14:12:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ window.addEventListener('load', () => {
     let totalSeconds;
 
     function init() {
-        totalSeconds = Math.floor((new Date('07/07/2023') - new Date()) / 1000);
+        totalSeconds = Math.floor((new Date('07/07/2024') - new Date()) / 1000);
         setTimeLeft();
         let interval = setInterval(() => {
             if (totalSeconds < 0) {
